2997ab4197aecf1f57a7b0453aa80d71dc750588
zone-mini <527821874@qq.com>
2022-06-08 19:58:49 +0800

    go

--- leetcode_go/lc300+最长递增子序列.go
+++ leetcode_go/lc300+最长递增子序列.go
@@ -1,1 +1,13 @@
-package leetcode_go
+package main
+
+func main() {
+
+}
+
+func lengthOfLIS(nums []int) int {
+	nums_length := len(nums)
+	dp := make([]int, nums_length)
+
+	for i := 0; i < nums_length; i++ {}
+	return 0
+}
